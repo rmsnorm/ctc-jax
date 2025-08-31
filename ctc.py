28459646908350df744157cf28c5a0dab614f3ea
rmsnorm <imp.apoorv@gmail.com>
2025-08-31 22:56:56 +0530

    Compute Forward-Backward in log-space. Add test for gradient computation.

--- ctc.py
+++ ctc.py
@@ -1,7 +1,7 @@
 """Implements the CTC objective as described in the CTC paper."""
 
 import numpy as np
-from scipy.special import logsumexp
+from scipy.special import logsumexp, log_softmax
 
 # X is a sequence of inputs [x1, x2, ...., xM]. Y is a sequence of symbols
 # [y1, y2, ..., yN], where N <= M. The CTC objective is used when the alignment
@@ -13,7 +13,7 @@ from scipy.special import logsumexp
 # where A is a valid alignment [a1, a2, ...aM] between X and Y, and
 # \textbf{A}_{X,Y} is the set of all valid alignments between X and Y.
 #
-# A few resources I followed to understand CTC and the forward-backward
+# A couple resources I followed to understand CTC and the forward-backward
 # algorithm.
 # - 1) The distillpub article on CTC is excellent: https://distill.pub/2017/ctc/
 # - 2) Prof. Bhiksha Raj's lecture: https://youtu.be/xwdBvlpZvXU
@@ -39,13 +39,13 @@ def create_extended_sequence(y, blank_idx):
 
 
 def forward_algo(
-    x_tv: np.array,
+    logprobs_tv: np.array,
     y: np.array,
     blank_idx: int,
     log_epsilon: float,
 ):
     """Forward computation of the forward-backward algo."""
-    T = x_tv.shape[0]
+    T = logprobs_tv.shape[0]
     K = y.shape[0]
 
     N = 2 * K + 1
@@ -53,29 +53,28 @@ def forward_algo(
 
     # initialize alpha of shape [T, N].
     log_alpha = [[log_epsilon] * N for _ in range(T)]
-    log_alpha[0][0] = np.log(x_tv[0, blank_idx])
-    log_alpha[0][1] = np.log(x_tv[0, s[1]])
+    log_alpha[0][0] = logprobs_tv[0, blank_idx]
+    log_alpha[0][1] = logprobs_tv[0, s[1]]
 
     for t in range(1, T):
-        log_alpha[t][0] = log_alpha[t - 1][0] + np.log(x_tv[t, s[0]])  # s[0] is blank
+        log_alpha[t][0] = log_alpha[t - 1][0] + logprobs_tv[t, s[0]]  # s[0] is blank
         for k in range(1, N):
             log_alpha[t][k] = logsumexp([log_alpha[t - 1][k], log_alpha[t - 1][k - 1]])
             if k >= 2 and s[k] != s[k - 2]:
                 log_alpha[t][k] = logsumexp([log_alpha[t][k], log_alpha[t - 1][k - 2]])
-            log_alpha[t][k] = log_alpha[t][k] + np.log(x_tv[t, s[k]])
+            log_alpha[t][k] = log_alpha[t][k] + logprobs_tv[t, s[k]]
 
     return np.array(log_alpha)
 
 
 def backward_algo(
-    x_tv: np.array,
+    logprobs_tv: np.array,
     y: np.array,
     blank_idx: int,
     log_epsilon: float,
-    # discount_current: bool = True,
 ):
     """Backward computation of the forward-backward algo."""
-    T = x_tv.shape[0]
+    T = logprobs_tv.shape[0]
     K = y.shape[0]
     N = 2 * K + 1
     s = create_extended_sequence(y, blank_idx)
@@ -89,41 +88,66 @@ def backward_algo(
         for k in range(N - 1, -1, -1):
             candidates = []
             # stay in same state
-            candidates.append(log_beta[t + 1][k] + np.log(x_tv[t + 1, s[k]]))
+            candidates.append(log_beta[t + 1][k] + logprobs_tv[t + 1, s[k]])
 
             # transition to next state
             if k + 1 < N:
-                candidates.append(
-                    log_beta[t + 1][k + 1] + np.log(x_tv[t + 1, s[k + 1]])
-                )
+                candidates.append(log_beta[t + 1][k + 1] + logprobs_tv[t + 1, s[k + 1]])
             if k + 2 < N and s[k] != s[k + 2]:
-                candidates.append(
-                    log_beta[t + 1][k + 2] + np.log(x_tv[t + 1, s[k + 2]])
-                )
+                candidates.append(log_beta[t + 1][k + 2] + logprobs_tv[t + 1, s[k + 2]])
             log_beta[t][k] = logsumexp(candidates)
 
     return np.array(log_beta)
 
 
 def compute_posterior_probs(
-    x_tv: np.array, y: np.array, blank_idx: int, log_epsilon: float = -100000.0
+    logprobs_tv: np.array, y: np.array, blank_idx: int, log_epsilon: float = -100000.0
 ):
-    log_alpha_tr = forward_algo(x_tv, y, blank_idx, log_epsilon)
-    log_beta_tr = backward_algo(x_tv, y, blank_idx, log_epsilon)
+    log_alpha_tr = forward_algo(logprobs_tv, y, blank_idx, log_epsilon)
+    log_beta_tr = backward_algo(logprobs_tv, y, blank_idx, log_epsilon)
 
     # posterior probs
     log_gamma_tr = log_alpha_tr + log_beta_tr
     norms = logsumexp(log_gamma_tr, axis=-1, keepdims=True)
     log_gamma_tr = log_gamma_tr - norms
 
-    return log_gamma_tr
+    return log_gamma_tr, -logsumexp(log_alpha_tr[-1, -2:], axis=-1)
 
 
-def compute_ctc(x_tv: np.array, y: np.array, blank_idx: int):
-    log_gamma_tr = compute_posterior_probs(x_tv, y, blank_idx)
+def compute_ctc(logits_tv: np.array, y: np.array, blank_idx: int):
+    logprobs_tv = log_softmax(logits_tv + 1e-8, axis=-1)
+
+    log_gamma_tr = compute_posterior_probs(logprobs_tv, y, blank_idx)
     s = create_extended_sequence(y, blank_idx)
-    divergence = -np.exp(log_gamma_tr) * np.log(x_tv[:, s])
+    divergence = -np.exp(log_gamma_tr) * logprobs_tv[:, s]
     return divergence.sum()
 
 
-# def compute_ctc_grad(x_tv: np.array, y_t: np.array, blank_idx: int):
+def compute_ctc_optax_equiv(
+    logits_tv: np.array, y: np.array, blank_idx: int, log_epsilon: float = -100000.0
+):
+    logprobs_tv = log_softmax(logits_tv + 1e-8, axis=-1)
+    log_gamma, loss = compute_posterior_probs(logprobs_tv, y, blank_idx, log_epsilon)
+    gamma = np.exp(log_gamma)
+
+    s = create_extended_sequence(y, blank_idx)
+    V = np.arange(logits_tv.shape[1])
+
+    A = (V[:, np.newaxis] == s).astype(int)
+
+    # gradient of loss wrt probs P = A * Gamma / Probs
+    # gradient of probs P wrt logits = P(I - P)
+    # therefore gradient of loss wrt logits = A * Gamma * (I - P)
+    probs_tv = np.exp(logprobs_tv)
+
+    # Jacobian matrix of probs
+    dprobs_tv = -np.matmul(A, gamma.T).T / probs_tv
+    probs_tv_e = probs_tv[..., np.newaxis]
+    # Jacobian matrix of softmax
+    dprobs_logits_tvv = np.einsum(
+        "tve,tew->tvw", probs_tv_e, -probs_tv_e.transpose(0, 2, 1)
+    ) + np.apply_along_axis(np.diag, arr=probs_tv, axis=1)
+
+    # Actual Jacobian of loss wrt logits
+    dlogits_tv = np.einsum("tv,tvw -> tw", dprobs_tv, dprobs_logits_tvv)
+    return loss, dlogits_tv
--- ctc_test.py
+++ ctc_test.py
@@ -4,16 +4,22 @@ import unittest
 
 import ctc
 import numpy as np
-from scipy.special import logsumexp
+from scipy.special import logsumexp, log_softmax
 import optax
+import jax
+import jax.numpy as jnp
 
 
 class TestForwardBackward(unittest.TestCase):
     def test_probability_invariant(self):
         np.random.seed(123)
-        x_tv = np.random.dirichlet(alpha=[1.0] * 4, size=5)
+        x_tv = np.random.normal(0.0, 10000, (9, 4))
+        logprobs_tv = log_softmax(x_tv, axis=-1)
         y = np.array([0, 1, 1, 2, 2])
-        log_posterior_probs = ctc.compute_posterior_probs(x_tv, y, 3, -100000.0)
+        (
+            log_posterior_probs,
+            _,
+        ) = ctc.compute_posterior_probs(logprobs_tv, y, 3, -100000.0)
         self.assertTrue(np.all(log_posterior_probs <= 0))
         self.assertTrue(
             np.allclose(logsumexp(log_posterior_probs, axis=-1, keepdims=True), 0.0)
@@ -21,10 +27,11 @@ class TestForwardBackward(unittest.TestCase):
 
     def test_forward_backward_posterior_probs_same_at_all_time_steps(self):
         np.random.seed(123)
-        x_tv = np.random.dirichlet(alpha=[1.0, 1.0, 1.0, 1.0], size=5)
+        x_tv = np.random.normal(0.0, 10000, (9, 4))
+        logprobs_tv = log_softmax(x_tv, axis=-1)
         y = np.array([0, 1, 1, 2, 2])
-        log_alpha = ctc.forward_algo(x_tv, y, 3, -100000.0)
-        log_beta = ctc.backward_algo(x_tv, y, 3, -100000.0)
+        log_alpha = ctc.forward_algo(logprobs_tv, y, 3, -100000.0)
+        log_beta = ctc.backward_algo(logprobs_tv, y, 3, -100000.0)
 
         log_gamma = log_alpha + log_beta
 
@@ -47,30 +54,34 @@ class TestCTC(unittest.TestCase):
         # for target sequence with repetitions and of length N,we need minimum
         # 2*N-1 input sequence length. N for the actual symbols itself and N-1
         # for the blanks between repeated symbols.
-        x_tv_logits = np.random.normal(loc=0.0, scale=0.01, size=(1, 9, 4))
-        x_tv_probs = self.stable_softmax(x_tv_logits, axis=-1)
-
+        x_tv_logits = np.random.normal(loc=0.0, scale=1, size=(1, 9, 4))
         blank_id = 3
 
-        # optax actually just computes the NLL of the target sequence. For this,
-        # we need to compare only the log_probs from alpha.
-        log_alpha = ctc.forward_algo(x_tv_probs[0], y, blank_id, log_epsilon=-100000.0)
-
-        #
-        logit_paddings = np.zeros((1, x_tv_logits.shape[1]))
-        label_paddings = np.zeros((1, 5))
-        expected_loss = optax.ctc_loss(
-            logits=x_tv_logits,
-            logit_paddings=logit_paddings,
-            labels=y[np.newaxis, ...],
-            label_paddings=label_paddings,
-            blank_id=blank_id,
-            log_epsilon=-100000.0,
+        # optax actually just computes the NLL of the target sequence.
+        loss, grad = ctc.compute_ctc_optax_equiv(
+            x_tv_logits[0], y, blank_id, log_epsilon=-100000.0
         )
 
-        self.assertAlmostEqual(
-            -logsumexp(log_alpha[-1, -2:]), expected_loss[0, ...], places=5
+        def loss_fn(logits, y, blank_id, log_epsilon):
+            logit_paddings = np.zeros((1, logits.shape[1]))
+            label_paddings = np.zeros((1, y.shape[0]))
+            return jnp.mean(
+                optax.ctc_loss(
+                    logits=logits,
+                    logit_paddings=logit_paddings,
+                    labels=y[np.newaxis, ...],
+                    label_paddings=label_paddings,
+                    blank_id=blank_id,
+                    log_epsilon=log_epsilon,
+                )
+            )
+
+        expected_loss, expected_grads = jax.value_and_grad(loss_fn)(
+            x_tv_logits, y, blank_id, -100000.0
         )
+
+        self.assertAlmostEqual(loss, expected_loss, places=5)
+        self.assertTrue(np.allclose(grad, expected_grads[0], atol=1e-6))
 
 
 if __name__ == "__main__":
